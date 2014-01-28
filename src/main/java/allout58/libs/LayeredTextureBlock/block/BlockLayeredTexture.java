22f6253ecf60ff07c97c279dace1862574156724
James Hollowell <jamesthollowell@gmail.com>
2014-01-27 22:28:13 -0500

    Stripped out unnecessary stuffs.

--- src/main/java/allout58/libs/LayeredTextureBlock/block/BlockLayeredTexture.java
+++ src/main/java/allout58/libs/LayeredTextureBlock/block/BlockLayeredTexture.java
@@ -1,13 +1,7 @@
 package allout58.libs.LayeredTextureBlock.block;
 
-import java.awt.image.BufferedImage;
-
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.texture.DynamicTexture;
-import net.minecraft.client.renderer.texture.IconRegister;
-import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.util.Icon;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -15,15 +9,15 @@ import cpw.mods.fml.relauncher.SideOnly;
 public abstract class BlockLayeredTexture extends Block
 {
     /**
-     * The blocks current render pass (0=base,1=overlay)
+     * The blocks current render pass (0=base,1=overlay).
      */
     public static int renderPass;
 
-    // public final TextureAtlasSprite blank = new
-    // TextureAS("layeredtexture_blank");
+    /**
+     * Icon used when a side doesn't need to be rendered on a pass.
+     */
     public Icon blank = new Icon()
     {
-
         @Override
         @SideOnly(Side.CLIENT)
         public int getIconWidth()
@@ -89,35 +83,11 @@ public abstract class BlockLayeredTexture extends Block
         }
 
     };
-//    private DynamicTexture blankTexture;
-//    private final int[] blankTextureData;
 
     public BlockLayeredTexture(int par1, Material par2Material)
     {
         super(par1, par2Material);
-//        BufferedImage bi = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
-//        for (int i = 0; i < 16; i++)
-//        {
-//            for (int j = 0; j < 16; j++)
-//            {
-//                bi.setRGB(i, j, 0);
-//            }
-//        }
-//        blankTexture = new DynamicTexture(bi);
-//        blankTextureData = blankTexture.getTextureData();
     }
-
-    // @Override
-    // public boolean renderAsNormalBlock()
-    // {
-    // return false;
-    // }
-
-    // @Override
-    // public int getRenderType()
-    // {
-    // return ClientProxy.BlockMultiLayerRenderer;
-    // }
 
     @Override
     public final boolean canRenderInPass(int pass)
@@ -131,50 +101,4 @@ public abstract class BlockLayeredTexture extends Block
     {
         return 1;
     }
-
-//    @Override
-//    @SideOnly(Side.CLIENT)
-//    public void registerIcons(IconRegister ir)
-//    {
-//        TextureManager tm = Minecraft.getMinecraft().getTextureManager();
-        // tm.getDynamicTextureLocation("layered_blank",
-        // blankTexture).getResourcePath();
-        // blank=
-
-        // this.blank.setFramesTextureData(Lists.newArrayList(new int[][] {
-        // blankTextureData}));
-        // this.blank.setIconWidth(16);
-        // this.blank.setIconHeight(16);
-        // if(ir instanceof TextureMap)
-        // System.out.println("Register success: "+((TextureMap)ir).setTextureEntry("layered_blank",
-        // this.blank));
-        // else
-        // System.out.println("Failed registration!");
-//    }
-
-    // @Override
-    // public Icon getBlockTexture(IBlockAccess world, int x, int y, int z, int
-    // side)
-    // {
-    // return this.getBlockTextureByPass(world, x, y, z, side,
-    // ClientProxy.renderPass);
-    // }
-
-    /**
-     * Gets the texture for a side for a render pass Override in subclasses to
-     * actually
-     * 
-     * @param world
-     * @param x
-     * @param y
-     * @param z
-     * @param side
-     * @param pass
-     * @return The icon to render
-     */
-    // public Icon getBlockTextureByPass(IBlockAccess world, int x, int y, int
-    // z, int side, int pass)
-    // {
-    // return this.blank;
-    // }
 }
